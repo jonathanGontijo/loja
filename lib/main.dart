7f027c11ff4b41ae2ec37105e612a82cec40f4de
jonathanGontijo <jonathan.ferreira1312@hotmail.com>
2026-07-25 17:32:07 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:loja/constants/global_variables.dart';
+import 'package:loja/features/auth/screens/auth_screen.dart';
+import 'package:loja/router/router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Loja',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: AuthScreen(),
+    );
+  }
+}
+
