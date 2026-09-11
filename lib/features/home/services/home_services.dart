048baf27e26634e0edcbef59112192213e3badd5
jonathanGontijo <jonathan.ferreira1312@hotmail.com>
2026-09-11 18:28:03 -0300

    fixed route product

--- lib/features/home/services/home_services.dart
+++ lib/features/home/services/home_services.dart
@@ -18,18 +18,21 @@ class HomeServices {
     List<Product> productList = [];
     try {
       http.Response res = await http.get(
-        Uri.parse('$uri/admin/get-products?category=$category'),
+        Uri.parse('$uri/api/products?category=$category'),
         headers: {
           'Content-Type': 'application/json; charset=UTF-8',
           'x-auth-token': userProvider.user.token,
         },
       );
+
       httpErrorHandle(
         response: res,
         context: context,
         onSuccess: () {
           for (int i = 0; i < jsonDecode(res.body).length; i++) {
-            productList.add(Product.fromJson(jsonDecode(res.body)[i]));
+            productList.add(
+              Product.fromJson(jsonEncode(jsonDecode(res.body)[i])),
+            );
           }
         },
       );
